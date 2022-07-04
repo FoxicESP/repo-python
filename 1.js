2faa88b950468df6258a09a848847cdcc6fc84f7
FoxicESP <foxice25552@gmail.com>
2022-07-04 21:45:24 +0500

    add script

---
+++ 1.js
@@ -0,0 +1,10 @@
+let namber = 4;
+const persone = "Alex";
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
---
+++ store.js
@@ -0,0 +1,19 @@
+const storeName = "Apple";
+const storeDescription = {
+    budget: 10000,
+    employess: ["Alex", "Dina", "Alice"],
+    products: {phone: 4000, box: 1000},
+    open: true
+}
+
+const storeName = 'My awesome store';
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['Alex', 'John', 'Sam'],
+    products: {
+        'soap': 20,
+        'water': 5
+    },
+    open: true
+}
